f717bd81ed31ab493840a470822ee8f12c8615bf
Usama Blavins <N0680788@my.ntu.ac.uk>
2017-10-27 21:53:21 +0000

    added sdi lab 4 work

---
+++ testProj/newFile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct Node {
+	int data;
+	Node* next;
+};
+
+int main() {    
+	Node *list, *current, *first, *last, *temp, *trail, *p, *q;
+	list = new Node;
+	list->data = 47;
+	temp = list;
+	first = list;
+	list = new Node;
+	
+	return 0;
+}
